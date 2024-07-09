c21d6153f949d0ab5b58dbf541fb5bff23c8d575
Jason Lin <jason200796@gmail.com>
2024-07-09 10:07:25 +0800

    LB v2.9.5 https://hackmd.io/@Fm7IQ7N2SXe_T7P3HKrU6A/rkA7bGqD0

---
+++ appv2.9.5/resetBotV2_9_5.py
@@ -0,0 +1,30 @@
+import subprocess
+import time
+
+
+
+
+
+child_process = None  # 初始化全局變數
+
+
+def start():
+    global child_process
+    child_process = subprocess.Popen(['python', file_name])
+    child_process.wait()
+
+should_start_process = input("start?(y/n): ").lower() == 'y'
+
+file_name = "appV2.9.5.py"   # input("輸入啟動檔案名稱> ")
+
+if should_start_process:
+
+    try:
+        while True:
+            start()
+            time.sleep(1)
+    except Exception as e:
+        print(e)
+    except KeyboardInterrupt:
+        child_process.kill()
+        exit()
