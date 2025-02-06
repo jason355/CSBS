3dde6d9df707867d42f536e9e74cf6bd710a2ba5
Jason Lin <jason200796@gmail.com>
2025-02-06 15:52:56 +0800

    fix: LB/resetBot.py path from '\' to '/'

--- LB/resetBot.py
+++ LB/resetBot.py
@@ -8,17 +8,17 @@ import json
 child_process = None  # 初始化全局變數
 
 
-relative_path = ".\\appV3.0.2.py"
+relative_path = "./appV3.0.2.py"
 script_dir = os.path.dirname(os.path.abspath(__file__))
 absolute_path = os.path.join(script_dir, relative_path)
 absolute_path = str(Path(absolute_path))
 
 
-with open(".\\config.json","r", encoding="utf-8") as f:
+with open("./config.json","r", encoding="utf-8") as f:
     config = json.load(f)
 
 
-with open(".\\config.json","w", encoding="utf-8") as f:
+with open("./config.json","w", encoding="utf-8") as f:
     config["Dynamic"]["initial_start"] = True
     f.write(json.dumps(config, indent = 4))
 
@@ -30,7 +30,6 @@ def start():
 
 
 if True:
-
     try:
         while True:
             start()
