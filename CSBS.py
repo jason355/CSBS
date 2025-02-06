03889814cb3078d9451349898790c2c1a719bf82
Jason Lin <jason200796@gmail.com>
2025-02-06 16:53:28 +0800

    update:csbs.py Add file path management to handle different systems

--- CSBS.py
+++ CSBS.py
@@ -1,16 +1,31 @@
 import subprocess
 import time
+import os
 
+# 取得目前腳本所在資料夾
+base_dir = os.path.dirname(os.path.abspath(__file__))
+print(base_dir)
 
 ngrok_command = "ngrok http 5000"
-script1 = "./LB/resetBot.py"
-script2 = "./server/monitor_V1.2.py"
-process = subprocess.Popen(ngrok_command, creationflags=subprocess.CREATE_NEW_CONSOLE)
+ngrok_process = subprocess.Popen(ngrok_command, creationflags=subprocess.CREATE_NEW_CONSOLE)
 
 time.sleep(2)
 
-command1 = f"python {script1}"
-process1 = subprocess.Popen(command1, creationflags=subprocess.CREATE_NEW_CONSOLE)
+linebot = os.path.join(base_dir, "LB", "resetBot.py")
+linebot = "resetBot.py"
+linebot_cwd = os.path.join(base_dir, "LB")
+linebot_cmd = f"python {linebot}"
+linebot_process = subprocess.Popen(args=linebot_cmd, cwd=linebot_cwd, creationflags=subprocess.CREATE_NEW_CONSOLE)
 
-command2 = f"python {script2}"
-process2 = subprocess.Popen(command2, creationflags=subprocess.CREATE_NEW_CONSOLE)
+
+# 取得在Server 資料夾中的 monitor_Vxx.py
+server_cwd = os.path.join(base_dir, "server")
+prefix = "monitor_V" # 設定前綴
+file = [f for f in os.listdir(server_cwd) if f.startswith(prefix)]
+if len(file) != 1:
+    raise ValueError(f"found more than one file start with '{prefix}'")
+else:
+    server = file[0]
+
+server_cmd = f"python {server}"
+server_process = subprocess.Popen(server_cmd, cwd=server_cwd, creationflags=subprocess.CREATE_NEW_CONSOLE)
